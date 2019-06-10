6ecc0349195b5f89cef99b0d42d6de9fe52b9961
Lai Wei <lai.wei@rice.edu>
2019-06-10 17:57:11 +0000

    run script for theta

--- amg2013/test/run-theta.sh
+++ amg2013/test/run-theta.sh
@@ -5,6 +5,13 @@
 #COBALT -q default
 #debug-cache-quad
 
+for i in {1..2}
+do
+	aprun -n 4096 -N 64 -cc depth -d 1 -j 1 hpcrun -o hpctoolkit-amg2013-16x16x16-$i-measurements -e WALLCLOCK@2000 -t ./amg2013 -pooldist 0 -r 12 12 12 -P 16 16 16 &> output-4096-$i &
+	sleep 1
+done
+
+<<COMMENT1
 for i in {1..3}
 do
 	aprun -n 1728 -N 64 -cc depth -d 1 -j 1 hpcrun -o hpctoolkit-amg2013-12x12x12-$i-measurements -e WALLCLOCK@2000 -t ./amg2013 -pooldist 0 -r 12 12 12 -P 12 12 12 &
@@ -16,12 +23,18 @@ do
 	aprun -n 512 -N 64 -cc depth -d 1 -j 1 hpcrun -o hpctoolkit-amg2013-8x8x8-$i-measurements -e WALLCLOCK@2000 -t ./amg2013 -pooldist 0 -r 12 12 12 -P 8 8 8 &
 	sleep 1
 done
+COMMENT1
+
 
 wait
 
+for i in {1..2}
+do
+    aprun -n 4096 -N 64 -cc depth -d 1 -j 1 hpcprof-mpi -S amg2013.hpcstruct hpctoolkit-amg2013-16x16x16-$i-measurements &
+	sleep 1
+done
 
-
-
+<<COMMENT2
 for i in {1..3}
 do
     aprun -n 1728 -N 64 -cc depth -d 1 -j 1 hpcprof-mpi -S amg2013.hpcstruct hpctoolkit-amg2013-12x12x12-$i-measurements &
@@ -33,6 +46,7 @@ do
     aprun -n 512 -N 64 -cc depth -d 1 -j 1 hpcprof-mpi -S amg2013.hpcstruct hpctoolkit-amg2013-8x8x8-$i-measurements &
 	sleep 1
 done
+COMMENT2
 
 wait
 
